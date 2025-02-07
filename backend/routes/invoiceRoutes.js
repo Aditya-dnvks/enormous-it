f7b51ef36b0f88d8c0ac3c16bd43ef27bdc63b74
Aditya Dasari <aditya.dnvks@gmail.com>
2025-02-07 13:37:36 +0530

    update import of auth token middleware

--- backend/routes/invoiceRoutes.js
+++ backend/routes/invoiceRoutes.js
@@ -1,27 +1,7 @@
-const jwt = require("jsonwebtoken");
-
 const express = require("express");
 const getInvoiceData = require("../controllers/invoiceController");
+const authenticateToken = require("../controllers/authentication");
 const router = express.Router();
-const JWT_SECRET = process.env.JWT_SECRET;
-
-const authenticateToken = (req, res, next) => {
-  const token = req.header("Authorization")?.split(" ")[1];
-
-  console.log("token", token);
-
-  if (!token)
-    return res
-      .status(401)
-      .json({ message: "Access denied. No token provided." });
-
-  jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: "Invalid token" });
-
-    req.user = user;
-    next();
-  });
-};
 
 router.get("/", authenticateToken, getInvoiceData);
 
